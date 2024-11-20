1a338e16c394ad74d558d806ebc3749750b67e56
csziroz <107149563+csziroz@users.noreply.github.com>
2024-11-20 19:00:48 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 #from models import Scientist
 
+Scientist = ''
 DATABASE_URL = "sqlite:///./ai_sci_gen.db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
